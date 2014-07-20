502e482effe6c50a8adb9cc3880209ceb530a564
Orsi Lautner <cs.orsi@gmail.com>
2014-07-20 10:11:08 -0400

    Handle numbers in a file

--- mean.py
+++ mean.py
@@ -4,12 +4,8 @@ sum=0
 n=0
 
 #Sum input values
-for num in sys.stdin:
+for num in open('data.txt'):
 	sum += float(num)
 	n += 1
-
-for num in sys.stdin:
-        sum += float(num)
-        n -= 1
 
 print sum / n
